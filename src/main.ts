8f3c22050bcbd98adf85f5480cf842b8a89f9af9
usaidmasud <syaripmasud@gmail.com>
2022-06-14 10:16:03 +0800

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { APIPrefix } from './utils/common';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix(APIPrefix.Version);
   app.use(helmet());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(5000);
 }
